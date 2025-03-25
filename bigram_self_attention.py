f2a7a30ccdd120096aa426559621a78d23666030
ryan <ryansen87@gmail.com>
2025-03-25 07:43:45 -0400

    fixing bugs

--- bigram_self_attention.py
+++ bigram_self_attention.py
@@ -5,6 +5,7 @@ from self_attention import Head
 
 class Bigram(nn.Module):
     def __init__(self, vocab_size, n_embed=32, block_size=8):
+        super().__init__()
         self.block_size = block_size
         self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
         self.position_embedding_table = nn.Embedding(block_size, n_embed)
@@ -14,8 +15,8 @@ class Bigram(nn.Module):
     def forward(self, idx, targets=None):
         B, T = idx.shape
 
-        tok_emb = self.token_embedding_table[idx]
-        pos_emb = self.position_embedding_table[idx]
+        tok_emb = self.token_embedding_table(idx)
+        pos_emb = self.position_embedding_table(torch.arange(T))
 
         x = tok_emb + pos_emb
         x = self.sa_head(x)
@@ -34,7 +35,7 @@ class Bigram(nn.Module):
     def generate(self, idx, size):
 
         for _ in range(size):
-            idx_cropped = idx[:, -self.block_size]
+            idx_cropped = idx[:, -self.block_size:]
             logits, loss = self.forward(idx_cropped)
 
             logits = logits[:,-1,:] # only last prediction
--- self_attention_test.py
+++ self_attention_test.py
@@ -1,7 +1,7 @@
 import torch
 import nltk
 from nltk.corpus import treebank
-from ngram.NGram import NGram
+from bigram_self_attention import Bigram
 from data.vocab import encode, decode, tokens
 
 nltk.download('treebank')
@@ -18,10 +18,10 @@ block_size = 16
 def get_batch(batch_size=64):
     ix = torch.randint(len(train_data) - block_size, (batch_size,))
     x = torch.stack([train_data[i:i+block_size] for i in ix])
-    y = torch.stack([train_data[i+block_size] for i in ix])
+    y = torch.stack([train_data[i+1:i+block_size+1] for i in ix])
     return x, y
 
-model = NGram(block_size, len(tokens))
+model = Bigram(len(tokens), block_size=block_size)
 optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
 
 for step in range(10000):
